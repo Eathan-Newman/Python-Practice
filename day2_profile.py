3e0c2f4977f1785fa5a817b777c0ff14f43bc13a
Eathan-Newman <eathanpnewman@gmail.com>
2026-07-21 18:57:48 -0500

    Complete day 2 Python profile program
    Practice variables, user input, and formatted output f-strings.

---
+++ day2_profile.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+degree = input("What is your degree? ")
+career_goal = input("What career are you pursuing? ")
+
+print()
+print("----- Student Profile ------")
+print(f"Name: {name}")
+print(f"Degree: {degree}")
+print(f"Career goal: {career_goal}")
