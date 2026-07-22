15be9c93939d077dcb658c3735b06a16eb93e78c
Eathan-Newman <eathanpnewman@gmail.com>
2026-07-21 19:23:09 -0500

    Complete Day 2 Python exercises
    Practice variables, user input, and formatted output.

---
+++ day2_pet.py
@@ -0,0 +1,8 @@
+name = input("What is your pet's name? ")
+animal_type = input("What type of animal is your pet? ")
+pet_age = input("What is your pet's age? ")
+
+print( "----- Pet Profile -----")
+print(f"Name: {name}")
+print(f"Animal: {animal_type}")
+print(f"Age: {pet_age}")
